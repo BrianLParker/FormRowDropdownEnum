e05d8204eb01a823b049c63721353da83dbf8985
Brian Parker <brian.parker@hotmail.com.au>
2020-12-09 11:09:45 +1100

    Answer to question

--- TestFormRowComponents/Pages/FormRowDropdownEnum.razor.cs
+++ TestFormRowComponents/Pages/FormRowDropdownEnum.razor.cs
@@ -1,28 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 
-namespace TestFormRowComponents.Pages {
-  public partial class FormRowDropdownEnum<T> where T : Enum, IConvertible {
-    [Parameter]
-    public int Value { get; set; }
-
-    [Parameter]
-    public EventCallback<int> ValueChanged { get; set; }
-
-    [Parameter]
-    public string PropertyName { get; set; }
-
-    [Parameter]
-    public string Caption { get; set; }
-
-    [Parameter]
-    public string Icon { get; set; }
-
-    private async Task OnValueChanged(ChangeEventArgs cea) {
-      int val = (int)Enum.Parse(typeof(T), cea.Value?.ToString() ?? "0");
-      Value = val;
-      await ValueChanged.InvokeAsync(val);
+namespace TestFormRowComponents.Pages
+{
+    public partial class FormRowDropdownEnum<TEnum>
+    {
+        private TEnum _value;
+        [Parameter] public string PropertyName { get; set; }
+        [Parameter] public string Caption { get; set; }
+        [Parameter] public string Icon { get; set; }
+        [Parameter]
+        public TEnum Value
+        {
+            get => _value;
+            set
+            {
+                if (_value.Equals(value)) return;
+                _value = value;
+                if (ValueChanged.HasDelegate)
+                {
+                    ValueChanged.InvokeAsync(Value);
+                }
+            }
+        }
+        [Parameter(CaptureUnmatchedValues = true)]
+        public Dictionary<string, object> AdditionalAttributes { get; set; }
+        [Parameter] public RenderFragment<TEnum> ChildContent { get; set; }
+        [Parameter] public EventCallback<TEnum> ValueChanged { get; set; }
+        IEnumerable<TEnum> EnumValues => Enum.GetValues(typeof(TEnum)).Cast<TEnum>();
+        protected override void OnInitialized()
+        {
+            if (Value is not Enum) throw new ArgumentException("Value must be of type Enum");
+        }
     }
-  }
 }
